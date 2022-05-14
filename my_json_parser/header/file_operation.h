599f92cb73033486ede51a8edf73bc9b1e5046b8
curtaincall <curtaincall_99@163.com>
2022-05-14 12:34:59 +0800

    initialize with the test frame and the main part. The 'null' 'true' and 'false' part is completed.

---
+++ my_json_parser/header/file_operation.h
@@ -0,0 +1,6 @@
+#pragma once
+#ifndef FILE_OPERATION_H_
+#define FILE_OPERATION_H_
+
+
+#endif // !FILE_OPERATION_H_
---
+++ test/parser_test.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+#include "header/praser_lite.h"
+
+TEST(blaTest, test1) {
+	// 尝试写个测试，看看googletest的用法，学
+}
